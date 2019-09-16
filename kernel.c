ad38427005dc62c9d0cc595e43ea261aa422bc23
KaichaJ <45885733+KaichaJ@users.noreply.github.com>
2019-09-15 17:33:56 -0700

    fix kernel loader to receive current process tf_p

--- kernel.c
+++ kernel.c
@@ -89,6 +89,6 @@ void Kernel(tf_t *tf_p) {       // kernel runs
    //call Scheduler() to change run_pid if needed
    Scheduler();
    //Call Loader() to load the trapframe of the selected process
-   Loader(tf_p); 
+   Loader(pcb[run_pid].tf_p); 
 }
 
