bbe8d1ec482c6bcc0c4bc7a0ad65400a93417092
Kaicha Johnson <johnsok@athena.ecs.csus.edu>
2019-09-20 22:17:48 -0700

    Add Init process

--- proc.c
+++ proc.c
@@ -1,9 +1,10 @@
 // proc.c, 159
 // processes are coded here
 
-//need to include const-type.h and ext-data.h
-#include "ext-data.h"
-#include "const-type.h"
+#include "const-type.h"  // VGA_MASK_VAL
+#include "ext-data.h"    // sys_time_count
+#include "syscall.h"     // sys service calls
+#include "tools.h"       // Number2Str()
 
 
 
@@ -35,4 +36,40 @@ void Idle(void){
 			}
 		}
 	}
+}
+
+void Init(void) {  // Init, PID 1, asks/tests various OS services
+   //declare two integers: my_pid & os_time
+   int my_pid, os_time;
+   //declare two 20-char arrays: pid_str & time_str
+   char pid_str[20];
+   char time_str[20];
+
+   //call sys_get_pid() to get my_pid
+   my_pid = sys_get_pid();
+   //call Number2Str() to convert it to pid_str
+   Number2Str(my_pid, pid_str);
+
+   while(1) {
+      // call sys_write() to show "my PID is "
+	  sys_write("my PID is ");
+      // call sys_write() to show my pid_str
+	  sys_write(pid_str);
+      // call sys_write to show "... "
+	  sys_write("... ");
+      // call sys_sleep() to sleep for 1 second
+	  sys_sleep(1);
+      // call sys_get_time() to get current os_time
+	  os_time = sys_get_time();
+      // call Number2Str() to convert it to time_str
+	  Number2Str(os_time, time_str);
+      // call sys_write() to show "sys time is "
+	  sys_write("sys time is ");
+      // call sys_write() to show time_str
+	  sys_write(time_str);
+      // call sys_write to show "... "
+	  sys_write("... ");
+      // call sys_sleep() to sleep for 1 second
+	  sys_sleep(1);
+   }
 }
