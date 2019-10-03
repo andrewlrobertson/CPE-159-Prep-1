0d393b6dd7c550be1ab300c9182eb5a750395779
andrewlrobertson <drewrob35@gmail.com>
2019-10-03 16:55:19 -0700

    updated to phase 4

--- ext-data.h
+++ ext-data.h
@@ -14,5 +14,7 @@ extern que_t avail_que;
 extern que_t ready_que;
 extern pcb_t pcb[PROC_MAX];
 extern unsigned short *sys_cursor;  //like others, add the new sys_cursor newly declared in main.c
+extern unsigned sys_rand_count;
+extern mutex_t video_mutex;
 
 #endif
