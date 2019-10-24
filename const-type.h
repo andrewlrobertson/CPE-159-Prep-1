2f09123fb165a33b88806beb44c48ba6f520747d
andrewlrobertson <drewrob35@gmail.com>
2019-10-23 18:35:17 -0700

    added new defines for phase 6

--- const-type.h
+++ const-type.h
@@ -10,6 +10,8 @@
 #define TIMER_SERVED_VAL 0x60  // control code sent to PIC
 #define VGA_MASK_VAL 0x0f00    // bold face, white on black
 
+#define SIGCHLD 17
+#define SIGCONT 18
 #define TIME_MAX 310            // max timer count, then rotate process
 #define PROC_MAX 20             // max number of processes
 #define STACK_MAX 4096          // process stack in bytes
@@ -34,6 +36,8 @@
 #define SYS_UNLOCK_MUTEX 137
 #define SYS_EXIT 138
 #define SYS_WAIT 139
+#define SYS_SIGNAL 140
+#define SYS_KILL 141
 #define VIDEO_START (unsigned short *)0xb8000
 #define VIDEO_END ((unsigned short *)0xb8000 + 25 * 80)
 
